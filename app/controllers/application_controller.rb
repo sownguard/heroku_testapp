0b816ec451ccf3bbf145dcb71ac1b60769dae975
sownguard <sownguard@yandex.ru>
2020-01-11 23:19:43 +0300

    cowsay deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::Base
 	#protect_from_forgery with :exception
 	
 	def hello
-		render html: "hello"
+		#render html: "hello"
 	end
 
 
 	def index
+		#render html: "hello"
 	end
 
 
