54441866d75fe74e5244b5a31433a9d05006aebf
sownguard <sownguard@yandex.ru>
2020-01-12 17:08:39 +0300

    microposts-v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #root 'application#hello'
-  root 'application#index'
-  #root 'welcome#index'
+  root 'users#index'
 end
